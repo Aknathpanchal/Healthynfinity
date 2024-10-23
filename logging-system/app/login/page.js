4fd6825e10593df676ace6977ea485afe6274d58
Shreenath Panchal <100464802+Aknathpanchal@users.noreply.github.com>
2024-10-23 10:49:34 +0530

    Update page.js

--- logging-system/app/login/page.js
+++ logging-system/app/login/page.js
@@ -11,7 +11,7 @@ const Login = () => {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        const res = await fetch('/api/auth', {
+        const res = await fetch('/api/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password }),
